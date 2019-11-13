a01fae00e2fd937d1715416be8eb74ed0bf8d14b
Shinji KOBAYASHI <skoba@moss.gr.jp>
2019-11-13 23:25:15 +0900

    XML validation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,6 @@ require 'json'
 require 'json-schema'
 require 'json_schemer'
 require 'supports/json_matchers'
-#require 'supports/fhir_json_matcher'
-#require "rspec/json_expectations"
-#require 'rspec/json_matcher'
 
 ROOT_PATH = File.expand_path('../', __dir__)
 SCHEMA_LOCATION = File.join(ROOT_PATH, 'schema')
@@ -14,4 +11,5 @@ XML_SCHEMA = File.join(SCHEMA_LOCATION, 'xml', 'xml.xsd')
 JSON_SCHEMA = File.join(SCHEMA_LOCATION, 'json', 'fhir.schema.json')
 
 MEDICATION0320_JSON_EXAMPLE = File.join(SAMPLE_LOCATION, 'swg5_pharma', 'medicationexample0320.json')
-#RSpec.configuration.include RSpec::JsonMatcher
+MEDICATION0320_XML_EXAMPLE = File.join(SAMPLE_LOCATION, 'swg5_pharma', 'medicationexample0320.xml')
+
