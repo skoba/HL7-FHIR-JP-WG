14c67f429acf4366a1766a9326bd36c1e6bb0bb0
Shinji KOBAYASHI <skoba@moss.gr.jp>
2019-11-15 22:04:17 +0900

    refactored rspec

--- spec/medication_spec.rb
+++ spec/medication_spec.rb
@@ -2,24 +2,20 @@ RSpec.describe "Medication Resource Instance" do
   describe 'JSON Instance' do
     let(:json) { File.read(File.join(SAMPLE_LOCATION, 'swg5_pharma', 'jp_medication_example_1.json'))}
 
-    example 'Valid JSON' do
-      expect(json).to be_valid_json
-    end
-    
-    example 'Valid FHIR JSON format' do
-      expect(json).to be_valid_fhir_json
-    end
+    subject { json }
+
+    it { is_expected.to be_valid_json }
+
+    it { is_expected.to be_valid_fhir_json }
   end
 
   describe 'XML Instance' do
     let(:xml) { File.read(File.join(SAMPLE_LOCATION, 'swg5_pharma', 'jp_medication_example_1.xml')) }
-    
-    example 'Valid XML' do
-      expect(xml).to be_valid_xml
-    end
 
-    example 'Valid FHIR XML' do
-      expect(xml).to be_valid_fhir_xml
-    end
+    subject { xml }
+
+    it { is_expected.to be_valid_xml }
+
+    it { is_expected.to be_valid_fhir_xml }
   end
 end
